78909883396336cc88acf8a887391eb0ab6e720b
hindenbug <manoj.mk27@gmail.com>
2018-09-25 23:20:49 +0200

    Add doom emacs config

---
+++ emacs/.doom.d/config.el
@@ -0,0 +1,62 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+(setq doom-font (font-spec :family "Fira Code Retina" :size 14))
+(setq which-key-idle-delay 0)
+(toggle-frame-maximized)
+
+(require 'doom-themes)
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+(load-theme 'doom-one t)
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+;; Enable custom neotree theme (all-the-icons must be installed!)
+(doom-themes-neotree-config)
+;; or for treemacs users
+(doom-themes-treemacs-config)
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
+(after! neotree
+  (setq neo-theme 'icons))
+
+(after! company
+  (setq company-idle-delay 0))
+
+(after! doom-themes
+  (setq doom-neotree-file-icons t))
+
+(after! ivy
+  (setq ivy-re-builders-alist
+      '((t . ivy--regex-ignore-order))))
+
+(after! projectile
+  (projectile-mode)
+  (projectile-load-known-projects))
+
+(def-package! rust-mode
+  :mode "\\.rs$"
+  :config
+  (flycheck-mode))
+
+(def-package! lsp-rust
+  :after (lsp-mode lsp-ui rust-mode)
+  :config
+  (setq lsp-rust-rls-command '("rustup" "run" "stable" "rls"))
+  :hook
+  (rust-mode . lsp-rust-enable))
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(height . 80))
+(add-to-list 'default-frame-alist '(width . 200))
+
+;; Fancy titlebar for MacOS
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+
+;; Show matching parens
+(setq show-paren-delay 0)
+(show-paren-mode 1)
