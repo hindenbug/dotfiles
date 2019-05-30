60e5b90b562273237a9035eca0e85e556f472981
hindenbug <manoj.mk27@gmail.com>
2019-05-30 20:47:08 +0200

    Fix doom config updates

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -51,9 +51,10 @@
        electric          ; smarter, keyword-based electric-indent
        ;eshell            ; a consistent, cross-platform shell (WIP)
        ;hideshow          ; basic code-folding support
-       imenu             ; an imenu sidebar and searchable code index
-       term              ; terminals in Emacs
        vc                ; version-control and Emacs, sitting in a tree
+
+       :term
+       term
 
        :tools
        (lookup           ; helps you navigate your code and documentation
